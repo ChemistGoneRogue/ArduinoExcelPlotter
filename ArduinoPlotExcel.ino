37b217ccf0374447379428e0c1e7b3863ad8e462
ChemistGoneRogue <63308568+ChemistGoneRogue@users.noreply.github.com>
2020-04-15 22:55:50 +0200

    Update ArduinoPlotExcel.ino

--- ArduinoPlotExcel.ino
+++ ArduinoPlotExcel.ino
@@ -8,6 +8,6 @@ void loop() {
   //Just plotting 0-19 and starting over
   for (int i = 0; i < 20; i++) {
     Serial.println(i); //the Serial.println() is the part needed to send data to the plotter
+    delay(500); //delay so the plotter has time to plot (can be way less than 500)  
   }
 }
-
