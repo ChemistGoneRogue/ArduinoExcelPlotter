3881cdf18b05b91b0afee2a305d4db82e64f8a4f
ChemistGoneRogue <63308568+ChemistGoneRogue@users.noreply.github.com>
2020-04-15 22:45:47 +0200

    Update ArduinoPlotExcel.ino

--- ArduinoPlotExcel.ino
+++ ArduinoPlotExcel.ino
@@ -1,6 +1,6 @@
 void setup() {
 
-Serial.begin(9600); //Any baudrate is fine
+  Serial.begin(9600); //Any baudrate is fine
 
 }
 void loop() {
@@ -8,4 +8,6 @@ void loop() {
   //Just plotting 0-19 and starting over
   for (int i = 0; i < 20; i++) {
     Serial.println(i); //the Serial.println() is the part needed to send data to the plotter
+  }
 }
+
